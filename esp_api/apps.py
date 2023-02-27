e874037ab1d4afd6ceb7a9c004334867fdd48db5
CHADLI KOUIDER <110521665+Chadlikouider@users.noreply.github.com>
2023-02-27 10:43:11 +0800

    Delete esp_api directory

--- esp_api/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class EspApiConfig(AppConfig):
-    name = 'esp_api'
--- esp_api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from esp_api import views
-
-urlpatterns = [
-    path('', views.index,name="esp_api"),
-    path("receive_data/", views.receive_data, name="receice_data"),
-]
